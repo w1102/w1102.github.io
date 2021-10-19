5f11441dcd3dfcb67a91291dc2c0f486acdd7baa
w1102 <tranwang1102@gmail.com>
2021-10-20 00:26:36 +0700

    edit url routing

--- script/url-routing.js
+++ script/url-routing.js
@@ -1,31 +1,30 @@
 var urlFull = [{"id":"home","urlHash":"home"},{"id":"target","urlHash":"muc-tieu"},{"id":"standard","urlHash":"chuan-dau-ra"},{"id":"method","urlHash":"pp-giang-day"},{"id":"rate","urlHash":"pp-danh-gia"},{"id":"time-table","urlHash":"tiet-hoc"},{"id":"location","urlHash":"phong-hoc"}]
 
 function FirstUrlRouting() {
-	let currentUrlHash = window.location.hash.substring(1)
+	let currentUrlHash = window.location.hash.substring(2)
 	if (currentUrlHash == '') {
-		window.location.replace('/#home');
+		window.location.replace('/#/home');
 		switchingMenu('home')
 	} else {
 		switchingMenu(getID(currentUrlHash))
 	}
 }
 
-function getID(hash) {
-	for (let i = 0; i < urlFull.length; i++) {
-		if (urlFull[i].urlHash == hash) {
-			return urlFull[i].id
-			console.log(urlFull)
+function getID(urlHash) {
+	for (let url of urlFull) {
+		if (url.urlHash == urlHash) {
+			return url.id
 		}
 	}
 	return ''
 }
 
 function setUrl(id) {
-	let currentUrlHash = window.location.hash.substring(1)
+	let currentUrlHash = window.location.hash.substring(2)
 	if (getID(currentUrlHash) != id) {
-		for (let i = 0; i < urlFull.length; i++) {
-			if (urlFull[i].id == id) {
-				window.location.replace('/#' + urlFull[i].urlHash)
+		for (let url of urlFull) {
+			if (url.id == id) {
+				window.location.replace('/#/' + url.urlHash)
 			}
 		}
 	}
